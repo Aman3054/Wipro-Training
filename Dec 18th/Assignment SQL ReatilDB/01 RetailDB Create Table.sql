cb1f098f8eaaaa0657d41dd46b7c19d024431ec1
Aman Goswami <amangoswami@gmail.com>
2025-12-18 16:32:03 +0530

    Assignment

---
+++ Dec 18th/Assignment SQL ReatilDB/01 RetailDB Create Table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Product (
+	ProdId INT PRIMARY KEY,
+	ProdName VARCHAR(100),
+	Price INT
+);
+CREATE TABLE Orders (
+	OrderId INT PRIMARY KEY,
+	ProdId INT,
+	Quantity INT
+);
---
+++ Dec 18th/Assignment SQL ReatilDB/03 RetailDB Inner Join, Left Join, Right Join.sql
@@ -0,0 +1,14 @@
+SELECT Product.ProdId, ProdName, Price, OrderId, Quantity
+FROM Product
+INNER JOIN Orders
+	ON Product.ProdId = Orders.ProdId;
+
+SELECT Product.ProdId, ProdName, Price, OrderId, Quantity
+FROM Product
+LEFT JOIN Orders
+	ON Product.ProdId = Orders.ProdId;
+
+SELECT Product.ProdId, ProdName, Price, OrderId, Quantity
+FROM Product
+RIGHT JOIN Orders
+	ON Product.ProdId = Orders.ProdId;
